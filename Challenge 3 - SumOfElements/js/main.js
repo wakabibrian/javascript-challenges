3d972245a7afb4841f21c02ac65f162f17b78c43
wakabibrian <wakabibrian95@gmail.com>
2026-04-27 08:52:47 +0300

    Add setup for challenge 3 - Sum Of All Elements in an array

---
+++ Challenge 3 - SumOfElements/js/main.js
@@ -0,0 +1,4 @@
+//======== Challenge 3: Sum of All Elements In an Array ============== //
+// Write a function sumArray(arr) that returns the sum of all elements
+// in an array. The function expects a parameter arr which is an array of
+// elements (use forEach or reduce array methods)
