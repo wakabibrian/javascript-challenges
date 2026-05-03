66fc065cc99889ab63c85b668392a07879a02635
wakabibrian <wakabibrian95@gmail.com>
2026-05-03 05:56:35 +0300

    Finish challenge 5

--- Challenge 5 - Assorted Object/js/main.js
+++ Challenge 5 - Assorted Object/js/main.js
@@ -4,75 +4,26 @@
 // The value for evens is an array of sorted even numbers, the value for odds is a list of
 // sorted odd numbers, and chars is a list of sorted single-character strings.
 
-// function assorted(arr) {
-//   assortedObj = {
-//     evens: [],
-//     odds: [],
-//     chars: [],
-//   };
+function assorted(arr) {
+  assortedObj = {
+    evens: [],
+    odds: [],
+    chars: [],
+  };
 
-//   for (el of arr) {
-//     if (typeof el === "number" || typeof el === "float") {
-//       if (el % 2 === 0) {
-//         assortedObj["evens"].push(el);
-//       } else {
-//         assortedObj["odds"].push(el);
-//       }
-//     } else if (typeof el === "string") {
-//       assortedObj["chars"].push(el);
-//     }
-//   }
+  for (el of arr) {
+    if (typeof el === "number" || typeof el === "float") {
+      if (el % 2 === 0) {
+        assortedObj["evens"].push(el);
+      } else {
+        assortedObj["odds"].push(el);
+      }
+    } else if (typeof el === "string") {
+      assortedObj["chars"].push(el);
+    }
+  }
 
-//   return assortedObj;
-// }
+  return assortedObj;
+}
 
-// console.log(assorted([1, 2, 4, 8.8, 2.4, "s", "t"]));
-
-//======== DOM - CRUD ============== //
-// let name = document.getElementById("name");
-// name.textContent = "Wakabi Brian";
-
-// Numbers
-let yearOfBirth = 1995;
-const numberOfEggs = 36;
-// let age = 25;
-// console.log(typeof yearOfBirth);
-// console.log(typeof age);
-
-// String
-let dateOfBirth = "1995";
-let place = "Kampala";
-place = "Nalya";
-
-// boolean
-let isBright = true;
-let isShort = false;
-// console.log(typeof isBright);
-
-// let x = 12 === 0;
-// console.log(x);
-
-// console.log("A" < "a");
-
-// Null & Undefined
-let age = null;
-console.log(age);
-
-// Primitive & Non-primitive
-let number = 24;
-let name = "Lionel";
-
-// array
-let arry = ["Wakabi", "Esther", 30, 40, false];
-console.log(arry[3]);
-console.log(arry[1]);
-
-// objects
-let student = {
-  name: "Lionel",
-  marks: 98,
-  isMale: true,
-};
-
-console.log(student.name);
-console.log(student.marks);
+console.log(assorted([1, 2, 4, 8.8, 2.4, "s", "t"]));
